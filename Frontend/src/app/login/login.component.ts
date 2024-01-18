769cea04cd296d7e4564aeb9a67cf4f46021a37c
Rahul-Wani <rahulwani3344@gmail.com>
2024-01-18 18:48:18 +0530

    Git init

---
+++ Frontend/src/app/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs';
+import { AuthenticationService } from '../authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  submitted: boolean = false;
+  emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  passwordRegex: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).{8,20}$/;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(this.emailRegex)]],
+      password: ['', [Validators.required, Validators.pattern(this.passwordRegex)]],
+      role: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.authService.loginUser(this.loginForm.value).subscribe({
+      next: response => {
+      alert("You have logged in successfully!");
+      sessionStorage.setItem('token', JSON.stringify(response));
+      this.router.navigateByUrl('/dashboard');
+    },
+    error: (error) => {
+      console.log(error);
+      alert(error?.error.error);
+    }});
+
+  }
+}
